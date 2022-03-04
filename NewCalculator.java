84d637fd9f72ac920ffb5eefdaf95e6f4947a549
uygaronaterol <94759671+uygaronaterol@users.noreply.github.com>
2022-03-04 11:37:55 +0300

    fix divising operation

--- NewCalculator.java
+++ NewCalculator.java
@@ -20,9 +20,9 @@ public class NewCalculator {
     }
 
     public double Divide(double dividend, double divisor){
-        this.lastResult = dividend % divisor;
-        return this.lastResult;
-    }
+this.lastResult = dividend / divisor;
+return this.lastResult;
+}
     public double Multiply(double x, double y){
 this.lastResult = x * y;
 return this.lastResult;
